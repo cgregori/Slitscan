535a502bb563fca156dbebe8e0a012d89636cc5f
cgregori <cgregori@fordham.edu>
2024-04-24 02:32:44 -0400

    Add all webcam gizmos to repo

---
+++ GrayscaleMosaic/sketch.js
@@ -0,0 +1,48 @@
+let video;
+let vScale = 16;
+let scaleSlider;
+
+function setup() {
+  createCanvas(640, 480);
+  
+  // High dpi monitors mean more than one pixel per pixel.
+  // This scales the pixels back down to 1:1
+  pixelDensity(1);
+  video = createCapture(VIDEO);
+  
+  // Slider from 8-bit? to 32-bit?
+  scaleSlider = createSlider(8, 32, 16, 4);
+  
+}
+
+function draw() {
+  vScale = scaleSlider.value();
+  video.size(width/vScale, height/vScale);
+  
+  
+  // Let's us manip. pixels and gives access to pixels[]
+  video.loadPixels();
+  loadPixels();
+  
+  
+  
+  for(let x = 0; x < video.width; x++) {
+    for(let y = 0; y < video.height; y++) {
+      
+      // Multiply by 4 because pixels are stored [RGBA, RGBA, ...]
+      let index = (x + y * video.width) * 4;
+      r = video.pixels[index+0];
+      g = video.pixels[index+1];
+      b = video.pixels[index+2];
+      a = video.pixels[index+3];
+      
+      // Simple Grayscale - Average of Sum of value.
+      let bright = (r+g+b)/3;
+      
+      fill(bright);
+      rect(x*vScale, y*vScale, vScale, vScale);
+    }
+  }
+  
+  video.updatePixels();
+}
